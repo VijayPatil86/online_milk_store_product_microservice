f9f45888d10bffb9a0da301a9426216d035b8ae3
VijayPatil86 <vijipatil090386@gmail.com>
2024-09-12 18:45:48 +0530

    modified for HATEOAS

--- src/main/java/com/online_milk_store/product_service/bean/CategoryBean.java
+++ src/main/java/com/online_milk_store/product_service/bean/CategoryBean.java
@@ -1,11 +1,14 @@
 package com.online_milk_store.product_service.bean;
 
+import org.springframework.hateoas.RepresentationModel;
+
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
-@Getter @Builder @ToString
-public class CategoryBean {
+@Getter @Setter @Builder @ToString
+public class CategoryBean extends RepresentationModel<CategoryBean>{
 	private int categoryId;
 	private String categoryName;
 	private String categoryAvailable;
