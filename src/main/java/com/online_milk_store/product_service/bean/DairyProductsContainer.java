ad6696dfb4fc584b99f87224cba8f5a9c21576ad
VijayPatil86 <vijipatil090386@gmail.com>
2024-09-14 19:30:59 +0530

    initial commit

---
+++ src/main/java/com/online_milk_store/product_service/bean/DairyProductsContainer.java
@@ -0,0 +1,16 @@
+package com.online_milk_store.product_service.bean;
+
+import java.util.List;
+
+import org.springframework.hateoas.RepresentationModel;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @Builder @ToString
+public class DairyProductsContainer extends RepresentationModel<DairyProductsContainer>{
+	private DairyProductBean dairyProductBean;
+	private List<DairyProductBean> categoryBeans;
+}
