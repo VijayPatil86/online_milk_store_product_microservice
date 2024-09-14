b8614d4433e21c61c023de6480176cd4e3913097
VijayPatil86 <vijipatil090386@gmail.com>
2024-09-14 19:41:16 +0530

    initial commit

---
+++ src/main/java/com/online_milk_store/product_service/entity/DairyProductEntity.java
@@ -0,0 +1,30 @@
+package com.online_milk_store.product_service.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @Builder @NoArgsConstructor @AllArgsConstructor @ToString
+@Entity
+@Table(name = "DAIRY_PRODUCT")
+public class DairyProductEntity {
+	@Column(name = "DAIRY_PRODUCT_ID")
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int dairyProductId;
+
+	@Column(name = "DAIRY_PRODUCT_NAME")
+	private String dairyProductName;
+
+	@Column(name = "DAIRY_PRODUCT_AVAILABLE", insertable = false)
+	private String dairyProductAvailable;
+}
