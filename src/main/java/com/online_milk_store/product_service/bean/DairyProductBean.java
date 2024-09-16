bd59d59493bf554cec6f75c914fd8f142605b5e0
VijayPatil86 <vijipatil090386@gmail.com>
2024-09-16 16:27:19 +0530

    modifies class to hold HATEOAS links

--- src/main/java/com/online_milk_store/product_service/bean/DairyProductBean.java
+++ src/main/java/com/online_milk_store/product_service/bean/DairyProductBean.java
@@ -1,5 +1,7 @@
 package com.online_milk_store.product_service.bean;
 
+import org.springframework.hateoas.RepresentationModel;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,7 +10,7 @@ import lombok.Setter;
 import lombok.ToString;
 
 @Getter @Setter @Builder @NoArgsConstructor @AllArgsConstructor @ToString
-public class DairyProductBean {
+public class DairyProductBean extends RepresentationModel<DairyProductBean>{
 	private int dairyProductId;
 	private String dairyProductName;
 	private String dairyProductAvailable;
