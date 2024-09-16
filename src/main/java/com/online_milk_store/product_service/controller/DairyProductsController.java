fecb95871c186d09e3ece96f33f0989f5b1b84fd
VijayPatil86 <vijipatil090386@gmail.com>
2024-09-16 16:32:14 +0530

    added code for GET, POST requests

--- src/main/java/com/online_milk_store/product_service/controller/DairyProductsController.java
+++ src/main/java/com/online_milk_store/product_service/controller/DairyProductsController.java
@@ -13,11 +13,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.online_milk_store.product_service.bean.DairyProductBean;
 import com.online_milk_store.product_service.bean.DairyProductsContainer;
+import com.online_milk_store.product_service.exception.CategoryNotAvailableException;
+import com.online_milk_store.product_service.exception.DairyProductAlreadyExistsException;
+import com.online_milk_store.product_service.exception.DairyProductNotAvailableException;
 import com.online_milk_store.product_service.exception.DairyProductsNotAvailableException;
 import com.online_milk_store.product_service.service.DairyProductService;
 
@@ -43,13 +49,58 @@ public class DairyProductsController {
 	}
 
 	@GetMapping
-	public ResponseEntity<List<DairyProductBean>> getAllAvailableDairyProducts() {
+	public ResponseEntity<DairyProductsContainer> getAllAvailableDairyProducts() {
 		LOGGER.debug("DairyProductsController.getAllAvailableDairyProducts() --- START");
 		List<DairyProductBean> listAllAvailableDairyProductsBeans = dairyProductService.getAllAvailableDairyProducts();
-		LOGGER.info("DairyProductsController.getAllAvailableDairyProducts() --- listAllAvailableDairyProductsBeans: " + listAllAvailableDairyProductsBeans);
-		ResponseEntity<List<DairyProductBean>> responseEntity = new ResponseEntity<>(listAllAvailableDairyProductsBeans, HttpStatus.OK);
+		listAllAvailableDairyProductsBeans.stream()
+			.forEach(dairyProductBean -> dairyProductBean.add(
+					WebMvcLinkBuilder.linkTo(methodOn(DairyProductsController.class).getDairyProductById(dairyProductBean.getDairyProductId())).withRel("link_getDairyProductById")));
+		LOGGER.info("DairyProductsController.getAllAvailableDairyProducts() --- listAllAvailableDairyProductsBeans with HATEOAS link: " + listAllAvailableDairyProductsBeans);
+		DairyProductsContainer dairyProductsContainer = DairyProductsContainer.builder()
+				.dairyProductBeans(listAllAvailableDairyProductsBeans)
+				.build();
+		dairyProductsContainer.add(
+				WebMvcLinkBuilder.linkTo(methodOn(DairyProductsController.class).getAllAvailableDairyProducts()).withRel("link_getAllAvailableDairyProducts"));
+		LOGGER.info("DairyProductsController.getAllAvailableDairyProducts() --- dairyProductsContainer: " + dairyProductsContainer);
+		ResponseEntity<DairyProductsContainer> responseEntity = new ResponseEntity<>(dairyProductsContainer, HttpStatus.OK);
 		LOGGER.debug("DairyProductsController.getAllAvailableDairyProducts() --- END");
 		return responseEntity;
+	}
+
+	@PostMapping("/{categoryId}")
+	public ResponseEntity<DairyProductsContainer> saveDairyProduct(@PathVariable(name = "categoryId") int categoryId, @RequestBody DairyProductBean dairyProductBean) {
+		LOGGER.debug("DairyProductsController.saveDairyProduct() --- START");
+		LOGGER.info("DairyProductsController.saveDairyProduct() --- categoryId: " + categoryId);
+		LOGGER.info("DairyProductsController.saveDairyProduct() --- dairyProductBean to save: " + dairyProductBean);
+		DairyProductBean savedDairyProductBean = dairyProductService.saveDairyProduct(categoryId, dairyProductBean);
+		LOGGER.info("DairyProductsController.saveDairyProduct() --- saved dairyProductBean: " + savedDairyProductBean);
+		savedDairyProductBean.add(
+				WebMvcLinkBuilder.linkTo(methodOn(DairyProductsController.class).getDairyProductById(savedDairyProductBean.getDairyProductId())).withRel("link_getDairyProductById"));
+		LOGGER.info("DairyProductsController.saveDairyProduct() --- HATEOAS links to dairyProductBean are: " + savedDairyProductBean);
+		DairyProductsContainer dairyProductsContainer = DairyProductsContainer.builder().build();
+		dairyProductsContainer.setDairyProductBean(savedDairyProductBean);
+		dairyProductsContainer.add(
+				WebMvcLinkBuilder.linkTo(methodOn(DairyProductsController.class).getAllAvailableDairyProducts()).withRel("link_getAllAvailableDairyProducts"));
+		LOGGER.info("DairyProductsController.saveDairyProduct() --- dairyProductsContainer: " + dairyProductsContainer);
+		LOGGER.debug("DairyProductsController.saveDairyProduct() --- END");
+		return new ResponseEntity<>(dairyProductsContainer, HttpStatus.OK);
+	}
+
+	@GetMapping("/{dairyProductId}")
+	public ResponseEntity<DairyProductsContainer> getDairyProductById(@PathVariable(name = "dairyProductId") int dairyProductId) {
+		LOGGER.debug("DairyProductsController.getDairyProductById() --- START");
+		LOGGER.info("DairyProductsController.getDairyProductById() --- dairyProductId: " + dairyProductId);
+		DairyProductBean dairyProductBean = dairyProductService.getDairyProductById(dairyProductId);
+		dairyProductBean.add(
+				WebMvcLinkBuilder.linkTo(methodOn(DairyProductsController.class).getDairyProductById(dairyProductId)).withRel("link_getDairyProductById"));
+		LOGGER.info("DairyProductsController.getDairyProductById() --- dairyProductBean: " + dairyProductBean);
+		DairyProductsContainer dairyProductsContainer = DairyProductsContainer.builder()
+				.dairyProductBean(dairyProductBean)
+				.build();
+		dairyProductsContainer.add(
+				WebMvcLinkBuilder.linkTo(methodOn(DairyProductsController.class).getAllAvailableDairyProducts()).withRel("link_getAllAvailableDairyProducts"));
+		LOGGER.info("DairyProductsController.getDairyProductById() --- dairyProductsContainer: " + dairyProductsContainer);
+		return new ResponseEntity<>(dairyProductsContainer, HttpStatus.OK);
 	}
 
 	/** Exception Handling section **/
@@ -59,5 +110,29 @@ public class DairyProductsController {
 		LOGGER.info("DairyProductsController.handleDairyProductsNotAvailableException() --- Dairy Products Not Available, returning No Content 204");
 		LOGGER.debug("DairyProductsController.handleDairyProductsNotAvailableException() --- END");
 		return ResponseEntity.noContent().build();
+	}
+
+	@ExceptionHandler(value = {CategoryNotAvailableException.class})
+	public ResponseEntity<String> handleCategoryNotAvailableException(CategoryNotAvailableException categoryNotAvailableException) {
+		LOGGER.debug("DairyProductsController.handleCategoryNotAvailableException() --- START");
+		LOGGER.info("DairyProductsController.handleCategoryNotAvailableException() --- " + categoryNotAvailableException.getMessage() + ", returning No Content 204");
+		LOGGER.debug("DairyProductsController.handleCategoryNotAvailableException() --- END");
+		return new ResponseEntity<>(categoryNotAvailableException.getMessage(), HttpStatus.NO_CONTENT);
+	}
+
+	@ExceptionHandler(value = {DairyProductAlreadyExistsException.class})
+	public ResponseEntity<String> handleDairyProductAlreadyExistsException(DairyProductAlreadyExistsException dairyProductAlreadyExistsException) {
+		LOGGER.debug("DairyProductsController.handleDairyProductAlreadyExistsException() --- START");
+		LOGGER.info("DairyProductsController.handleDairyProductAlreadyExistsException() --- " + dairyProductAlreadyExistsException.getMessage() + ", returning Conflict 409");
+		LOGGER.debug("DairyProductsController.handleDairyProductAlreadyExistsException() --- END");
+		return new ResponseEntity<>(dairyProductAlreadyExistsException.getMessage(), HttpStatus.CONFLICT);
+	}
+
+	@ExceptionHandler(value = {DairyProductNotAvailableException.class})
+	public ResponseEntity<String> handleDairyProductNotAvailableException(DairyProductNotAvailableException dairyProductNotAvailableException) {
+		LOGGER.debug("DairyProductsController.handleDairyProductNotAvailableException() --- START");
+		LOGGER.info("DairyProductsController.handleDairyProductNotAvailableException() --- " + dairyProductNotAvailableException.getMessage() + ", returning No Content 204");
+		LOGGER.debug("DairyProductsController.handleDairyProductNotAvailableException() --- END");
+		return new ResponseEntity<>(dairyProductNotAvailableException.getMessage(), HttpStatus.NO_CONTENT);
 	}
 }
