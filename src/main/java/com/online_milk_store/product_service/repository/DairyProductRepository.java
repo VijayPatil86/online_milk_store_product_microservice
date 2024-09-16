1020410bb12e91cb8c23bc1cb7c51e8095714020
VijayPatil86 <vijipatil090386@gmail.com>
2024-09-16 16:37:03 +0530

    added method findByDairyProductNameIs to check dairy product existence

--- src/main/java/com/online_milk_store/product_service/repository/DairyProductRepository.java
+++ src/main/java/com/online_milk_store/product_service/repository/DairyProductRepository.java
@@ -1,6 +1,7 @@
 package com.online_milk_store.product_service.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,5 @@ import com.online_milk_store.product_service.entity.DairyProductEntity;
 
 public interface DairyProductRepository extends JpaRepository<DairyProductEntity, Integer> {
 	List<DairyProductEntity> findByDairyProductAvailableIs(String dairyProductAvailableStatus);
+	Optional<DairyProductEntity> findByDairyProductNameIs(String dairyProductName);
 }
