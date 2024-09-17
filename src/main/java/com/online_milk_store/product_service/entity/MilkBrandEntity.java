fcec0848962c1d361ac75b801bd2cea7900410da
VijayPatil86 <vijipatil090386@gmail.com>
2024-09-17 13:28:15 +0530

    initial commit

---
+++ src/main/java/com/online_milk_store/product_service/entity/MilkBrandEntity.java
@@ -0,0 +1,39 @@
+package com.online_milk_store.product_service.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @Builder @NoArgsConstructor @AllArgsConstructor @ToString
+@Entity
+@Table(name = "MILK_BRAND")
+public class MilkBrandEntity {
+	@Column(name = "MILK_BRAND_ID")
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int milkBrandId;
+
+	@Column(name = "MILK_BRAND_NAME")
+	private String milkBrandName;
+
+	@Column(name = "PACKAGING")
+	private String packaging;
+
+	@Column(name = "MILK_BRAND_AVAILABLE")
+	private String milkBrandAvailable;
+
+	@ManyToOne
+	@JoinColumn(name = "DAIRY_PRODUCT_ID")
+	private DairyProductEntity dairyProductEntity;
+}
