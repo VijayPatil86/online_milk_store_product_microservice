cd0875de128f007fe35590cef6abeef012b44d25
VijayPatil86 <vijipatil090386@gmail.com>
2024-09-20 17:47:19 +0530

    initial commit

---
+++ src/main/java/com/online_milk_store/product_service/bean/MilkBrandPurchaseBean.java
@@ -0,0 +1,17 @@
+package com.online_milk_store.product_service.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @Builder @NoArgsConstructor @AllArgsConstructor @ToString
+public class MilkBrandPurchaseBean {
+	private int detailId;
+	private float purchasePrice;
+	private int purchaseQuantity;
+	private float totalPurchasePrice;
+	private String purchaseDateTime;
+}
