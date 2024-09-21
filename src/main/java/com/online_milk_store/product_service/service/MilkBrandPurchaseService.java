8461de253d8dea8d2b50e6ba5eb53ac9e4e2b3bc
VijayPatil86 <vijipatil090386@gmail.com>
2024-09-21 12:02:34 +0530

    added code to save/update purchase, inventory transactions

--- src/main/java/com/online_milk_store/product_service/service/MilkBrandPurchaseService.java
+++ src/main/java/com/online_milk_store/product_service/service/MilkBrandPurchaseService.java
@@ -1,6 +1,7 @@
 package com.online_milk_store.product_service.service;
 
 import java.sql.Timestamp;
+import java.util.Optional;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -8,10 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.online_milk_store.product_service.bean.BusinessProperties;
 import com.online_milk_store.product_service.bean.MilkBrandPurchaseBean;
 import com.online_milk_store.product_service.entity.MilkBrandEntity;
+import com.online_milk_store.product_service.entity.MilkBrandInventoryEntity;
 import com.online_milk_store.product_service.entity.MilkBrandPurchaseEntity;
 import com.online_milk_store.product_service.exception.MilkBrandNotAvailableException;
+import com.online_milk_store.product_service.repository.MilkBrandInventoryRepository;
 import com.online_milk_store.product_service.repository.MilkBrandPurchaseRepository;
 import com.online_milk_store.product_service.repository.MilkBrandRepository;
 
@@ -21,10 +25,16 @@ public class MilkBrandPurchaseService {
 	static final private Logger LOGGER = LogManager.getLogger(MilkBrandPurchaseService.class);
 
 	@Autowired
+	private BusinessProperties businessProperties;
+
+	@Autowired
 	private MilkBrandRepository milkBrandRepository;
 
 	@Autowired
 	private MilkBrandPurchaseRepository milkBrandPurchaseRepository;
+
+	@Autowired
+	private MilkBrandInventoryRepository milkBrandInventoryRepository;
 
 	public void addPurchaseRecord(int milkBrandId, MilkBrandPurchaseBean milkBrandPurchaseBean) {
 		LOGGER.debug("MilkBrandPurchaseService.addPurchaseRecord() --- START");
@@ -45,6 +55,35 @@ public class MilkBrandPurchaseService {
 		milkBrandPurchaseEntity.setMilkBrandEntity(milkBrandEntity);
 		MilkBrandPurchaseEntity savedMilkBrandPurchaseEntity = milkBrandPurchaseRepository.save(milkBrandPurchaseEntity);
 		LOGGER.info("MilkBrandPurchaseService.addPurchaseRecord() --- savedMilkBrandPurchaseEntity: " + savedMilkBrandPurchaseEntity);
+		LOGGER.info("MilkBrandPurchaseService.addPurchaseRecord() --- finding inventory record for milk brand id: " + milkBrandId);
+		Optional<MilkBrandInventoryEntity> optionalMilkBrandInventoryEntity = milkBrandInventoryRepository.findByMilkBrandEntityMilkBrandId(milkBrandId);
+		if(optionalMilkBrandInventoryEntity.isEmpty()) {
+			LOGGER.info("MilkBrandPurchaseService.addPurchaseRecord() --- inventory record for milk brand id: " +
+					milkBrandId + " not exists, adding the record");
+			MilkBrandInventoryEntity milkBrandInventoryEntity = MilkBrandInventoryEntity.builder()
+					.currentQuantity(milkBrandPurchaseBean.getPurchaseQuantity())
+					.minimumQuantity(businessProperties.getQuantityMinimum())
+					.currentPurchasePrice(milkBrandPurchaseBean.getPurchasePrice())
+					.currentSellPrice(milkBrandPurchaseBean.getMilkBrandInventoryBean().getCurrentSellPrice())
+					.outOfStock("N")
+					.build();
+			LOGGER.info("MilkBrandPurchaseService.addPurchaseRecord() --- milkBrandInventoryEntity: " + milkBrandInventoryEntity);
+			milkBrandInventoryEntity.setMilkBrandEntity(milkBrandEntity);
+			MilkBrandInventoryEntity createdMilkBrandInventoryEntity = milkBrandInventoryRepository.save(milkBrandInventoryEntity);
+			LOGGER.info("MilkBrandPurchaseService.addPurchaseRecord() --- createdMilkBrandInventoryEntity: " + createdMilkBrandInventoryEntity);
+		} else {
+			MilkBrandInventoryEntity existingMilkBrandInventoryEntity = optionalMilkBrandInventoryEntity.get();
+			LOGGER.info("MilkBrandPurchaseService.addPurchaseRecord() --- existingMilkBrandInventoryEntity: " + existingMilkBrandInventoryEntity);
+			existingMilkBrandInventoryEntity.setCurrentPurchasePrice(milkBrandPurchaseBean.getPurchasePrice());
+			existingMilkBrandInventoryEntity.setOutOfStock("N");
+			existingMilkBrandInventoryEntity.setMinimumQuantity(businessProperties.getQuantityMinimum());
+			existingMilkBrandInventoryEntity.setCurrentQuantity(existingMilkBrandInventoryEntity.getCurrentQuantity() +
+					milkBrandPurchaseBean.getPurchaseQuantity());
+			existingMilkBrandInventoryEntity.setCurrentSellPrice(milkBrandPurchaseBean.getMilkBrandInventoryBean().getCurrentSellPrice());
+			existingMilkBrandInventoryEntity.setMilkBrandEntity(milkBrandEntity);
+			MilkBrandInventoryEntity updatedMilkBrandInventoryEntity = milkBrandInventoryRepository.save(existingMilkBrandInventoryEntity);
+			LOGGER.info("MilkBrandPurchaseService.addPurchaseRecord() --- updatedMilkBrandInventoryEntity: " + updatedMilkBrandInventoryEntity);
+		}
 		LOGGER.debug("MilkBrandPurchaseService.addPurchaseRecord() --- END");
 	}
 }
