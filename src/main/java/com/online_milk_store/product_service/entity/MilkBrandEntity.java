f07b885f8cb5b1708b621c48fa58a74c45072f35
VijayPatil86 <vijipatil090386@gmail.com>
2024-09-21 18:03:03 +0530

    removed un-used import

--- src/main/java/com/online_milk_store/product_service/entity/MilkBrandEntity.java
+++ src/main/java/com/online_milk_store/product_service/entity/MilkBrandEntity.java
@@ -2,7 +2,6 @@ package com.online_milk_store.product_service.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
